0a42d5c666b590db5fbe24badbc76c2dd1f7fff9
Igor Pessoal <castromann@live.com>
2024-02-09 08:03:04 -0300

    feat(routes): scaffold

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import AppProviders from "./AppProviders";
-import { StatusBar, Text } from "native-base";
+import { StatusBar } from "native-base";
+import Stacks from "./routes";
 
 export default function App() {
   return (
     <AppProviders>
-      <Text className="text-red-500 ">unikStore</Text>
+      <Stacks />
       <StatusBar />
     </AppProviders>
   );
