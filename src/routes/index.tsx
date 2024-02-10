92aca24d92ba816bd8604fca41e044dbe610c855
Igor Pessoal <castromann@live.com>
2024-02-10 14:26:10 -0300

    chore(routes): add interface and cart screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,32 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Cart from "@src/screens/Cart";
 import Home from "@src/screens/Home";
+import { AntDesign } from "@expo/vector-icons";
+import { RootStackParamList } from "./types";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Stacks = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <AntDesign
+                name="shoppingcart"
+                size={24}
+                onPress={() => {
+                  navigation.navigate("Cart");
+                }}
+              />
+            ),
+          })}
+        />
+        <Stack.Screen name="Cart" component={Cart} />
       </Stack.Navigator>
     </NavigationContainer>
   );
