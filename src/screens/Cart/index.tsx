1e785351f6819dbd726064213e41c8f44e2bd8b4
Igor Pessoal <castromann@live.com>
2024-02-10 16:19:06 -0300

    fix(cart-item): items losing height when a item is removed

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,28 +1,27 @@
 import ScreenLayout from "@src/layouts/ScreenLayout";
 import { type RootStackScreenComponent } from "@src/routes/types";
 import useCartStore from "@src/store/cart";
-import { Button, Center, FlatList, Text } from "native-base";
+import { Button, Center, FlatList, Text, View } from "native-base";
 import useCartController from "./controller";
 
 import { CartTotal } from "./Total";
 import { CartItem } from "./Item";
+import { Item } from "@src/@types/item";
 
 // eslint-disable-next-line no-empty-pattern
 const Cart = ({}: RootStackScreenComponent<"Cart">) => {
   const { cartTotal, handleOpenCheckout } = useCartController();
-  const cartList = useCartStore.getState().cartList;
-  const hasCartItems = useCartStore.getState().cartList?.length > 0;
+  const cartList = useCartStore((state) => state.cartList);
 
   const cartTotalValue = cartTotal();
+
+  const hasCartItems = cartList.length > 0;
 
   if (!hasCartItems) return <EmptyCart />;
 
   return (
     <ScreenLayout height="100%">
-      <FlatList
-        data={cartList}
-        renderItem={({ item }) => <CartItem item={item} />}
-      />
+      <CartListItems cartList={cartList} />
       <CartTotal formattedItemsTotal={cartTotalValue} />
       <Button
         className="bg-green-500 mx-4 rounded-xl mb-1"
@@ -41,3 +40,19 @@ const EmptyCart = () => (
     <Text fontWeight="bold">Carrinho vazio</Text>
   </Center>
 );
+
+interface CartListItemsProps {
+  cartList: Item[];
+}
+const CartListItems = ({ cartList }: CartListItemsProps) => {
+  return (
+    <FlatList
+      data={cartList}
+      renderItem={({ item }) => (
+        <View className="h-24">
+          <CartItem item={item} />
+        </View>
+      )}
+    />
+  );
+};
