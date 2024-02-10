50b5fc4e423a9f40655fea45e2d00234ea433464
Igor Pessoal <castromann@live.com>
2024-02-10 19:43:48 -0300

    feat(checkout-modal): new component

---
+++ src/components/CheckoutModal/index.tsx
@@ -0,0 +1,16 @@
+import { Modal } from "native-base";
+import { CheckoutForm } from "../CheckoutForm";
+
+interface CheckoutModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+export const CheckoutModal = ({ isOpen, onClose }: CheckoutModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} className="bg-black">
+      <Modal.Content className="bg-black">
+        <CheckoutForm />
+      </Modal.Content>
+    </Modal>
+  );
+};
