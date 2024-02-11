6cf7fe6eab0ed7abd3c55ceec2e46fcd7ea92ad7
Igor Pessoal <castromann@live.com>
2024-02-11 13:11:05 -0300

    test(mocks): item and items mock functions

---
+++ test/mocks/item.ts
@@ -0,0 +1,14 @@
+import { Item } from "@src/@types/item";
+
+export const buildItemMock = (id = 1): Item => ({
+  id,
+  name: `item-${id}`,
+  price: 10,
+  quantity: 1,
+  image: "",
+  description: "description",
+  category: "category",
+});
+
+export const buildItemsArrayMock = (length: number): Item[] =>
+  [...Array(length)].map((_, i) => buildItemMock(i));
