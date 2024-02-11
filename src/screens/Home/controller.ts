4790776bd6b94df547fac93bab6e4dd217083e0b
Igor Pessoal <castromann@live.com>
2024-02-11 13:05:00 -0300

    chore(home-controller): add search to filter logic

--- src/screens/Home/controller.ts
+++ src/screens/Home/controller.ts
@@ -5,9 +5,10 @@ export const useHomeController = () => {
   const selectedCategory = useItemStore((state) => state.selectedCategory);
   const selectedItem = useItemStore((state) => state.selectedItem);
   const getFilteredItems = useItemStore((state) => state.getFilteredItems);
+  const search = useItemStore((state) => state.search);
 
   const handleGetFilteredItems = () => {
-    return getFilteredItems(selectedCategory);
+    return getFilteredItems(selectedCategory, search);
   };
 
   return {
