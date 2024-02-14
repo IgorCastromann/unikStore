f84c0053be241039afd05bd91407f64658e44afe
Igor Pessoal <castromann@live.com>
2024-02-14 15:20:01 -0300

    test(card-section): new tests

---
+++ src/components/CheckoutForm/CardSection/CardSection.test.tsx
@@ -0,0 +1,20 @@
+import { act, fireEvent } from "@testing-library/react-native";
+import { renderWithProvider } from "@test/render";
+import { CardSectionForm } from ".";
+
+const mockSetIsValid = jest.fn();
+
+describe("<CardSectionForm />", () => {
+  it("find submit button on validaded state", () => {
+    const { getByTestId } = renderWithProvider(
+      <CardSectionForm publicKey="" setIsValid={mockSetIsValid} />,
+    );
+
+    act(() => {
+      const cvv = getByTestId("cvv");
+      fireEvent.changeText(cvv, "123");
+    });
+
+    expect(mockSetIsValid).toHaveBeenCalledWith(true);
+  });
+});
