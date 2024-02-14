3029d96eddb9c18a8b788611a5cc8321ebb2a168
Igor Pessoal <castromann@live.com>
2024-02-14 15:00:29 -0300

    refactor(checkout-form): split into small components and add address

--- src/components/CheckoutForm/controller.ts
+++ src/components/CheckoutForm/controller.ts
@@ -1,20 +1,36 @@
+import { NavigationRoutes } from "@src/routes/types";
 import useCartStore from "@src/store/cart";
 import { useState } from "react";
 
 const PUBLIC_KEY = process.env.EXPO_PUBLIC_FRAMES_REACT_NATIVE_KEY;
 
 const useCheckoutFormController = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const publicKey = PUBLIC_KEY!;
   const [isValid, setIsValid] = useState(false);
+  const checkoutMutation = useCartStore((state) => state.checkoutCart());
+  const clearCart = useCartStore((state) => state.clearCart);
 
   const totalCartValue = useCartStore((state) =>
     state.getTotalValueCartItems(),
   );
 
-  const checkoutCart = useCartStore((state) => state.checkoutCart);
+  const handleCheckout = (navigation: NavigationRoutes | undefined) => {
+    const cartList = useCartStore.getState().cartList;
 
-  const handleCheckout = () => {
-    checkoutCart();
+    setIsLoading(true);
+    try {
+      checkoutMutation.mutate(cartList);
+      setTimeout(() => {
+        clearCart();
+        alert("Compra realizada com sucesso");
+        navigation?.navigate("Home");
+        setIsLoading(false);
+      }, 2000);
+    } catch (error) {
+      alert("Ops! Algo deu errado, tente novamente mais tarde.");
+      setIsLoading(false);
+    }
   };
 
   return {
@@ -23,6 +39,7 @@ const useCheckoutFormController = () => {
     isValid,
     setIsValid,
     publicKey,
+    isLoading,
   };
 };
 
--- src/components/CheckoutForm/index.tsx
+++ src/components/CheckoutForm/index.tsx
@@ -1,97 +1,37 @@
-import {
-  Frames,
-  CardNumber,
-  ExpiryDate,
-  Cvv,
-  SubmitButton,
-} from "frames-react-native";
-import { View } from "native-base";
-import { StyleSheet } from "react-native";
+import { Button, Divider, VStack } from "native-base";
 import useCheckoutFormController from "./controller";
+import { NavigationRoutes } from "@src/routes/types";
+import { CardSectionForm } from "./CardSection";
+import { AdressSection } from "./AdressSection";
 
-export const CheckoutForm = () => {
-  const { handleCheckout, isValid, publicKey, setIsValid, totalCartValue } =
-    useCheckoutFormController();
+interface CheckoutFormProps {
+  navigation: NavigationRoutes | undefined;
+}
+export const CheckoutForm = ({ navigation }: CheckoutFormProps) => {
+  const {
+    handleCheckout,
+    isValid,
+    publicKey,
+    setIsValid,
+    totalCartValue,
+    isLoading,
+  } = useCheckoutFormController();
 
   return (
-    <Frames
-      config={{
-        debug: true,
-        publicKey,
-      }}
-      cardTokenized={() => {}}
-      frameValidationChanged={(e) => {
-        setIsValid(e.isValid);
-      }}
-    >
-      <CardNumber style={styles.cardNumber} placeholderTextColor="#9898A0" />
+    <VStack className="h-full">
+      <AdressSection />
+      <Divider className="my-4" />
+      <CardSectionForm publicKey={publicKey} setIsValid={setIsValid} />
 
-      <View style={styles.dateAndCode}>
-        <ExpiryDate style={styles.expiryDate} placeholderTextColor="#9898A0" />
-        <Cvv style={styles.cvv} placeholderTextColor="#9898A0" testID="cvv" />
-      </View>
-
-      <SubmitButton
-        title={isValid ? `Pagar R$ ${totalCartValue}` : "Invalid card"}
-        style={styles.button}
-        textStyle={styles.buttonText}
+      <Button
+        className="bg-blue-500 h-[50] text-white rounded-sm mt-4"
         disabled={!isValid}
-        onPress={handleCheckout}
+        onPress={() => handleCheckout(navigation)}
         testID="submit-payment-button"
-      />
-    </Frames>
+        isLoading={isLoading}
+      >
+        {isValid ? `Pagar R$ ${totalCartValue}` : "Invalid card"}
+      </Button>
+    </VStack>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000000",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    paddingTop: 80,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  dateAndCode: {
-    marginTop: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  cardNumber: {
-    fontSize: 18,
-    height: 50,
-    color: "#FEFFFF",
-    backgroundColor: "#1B1C1E",
-    borderColor: "#3A4452",
-    borderRadius: 5,
-    borderWidth: 0,
-  },
-  expiryDate: {
-    fontSize: 18,
-    height: 50,
-    width: "48%",
-    color: "#FEFFFF",
-    backgroundColor: "#1B1C1E",
-    borderWidth: 0,
-  },
-  cvv: {
-    fontSize: 18,
-    height: 50,
-    width: "48%",
-    color: "#FEFFFF",
-    backgroundColor: "#1B1C1E",
-    borderWidth: 0,
-  },
-  button: {
-    height: 50,
-    borderRadius: 5,
-    marginTop: 20,
-    justifyContent: "center",
-    backgroundColor: "#4285F4",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-  },
-});
