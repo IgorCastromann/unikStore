01ecb452ad295b7022b0fefaeacc3ec83dc71e5b
Igor Pessoal <castromann@live.com>
2024-02-14 15:46:09 -0300

    test: mock axios to avoid errors in jest

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -3,6 +3,8 @@ import { extendTheme, NativeBaseProvider } from "native-base";
 import { type FC, type PropsWithChildren } from "react";
 import { queryClient } from "./queries";
 
+jest.mock("axios");
+
 const inset = {
   frame: {
     x: 0,
