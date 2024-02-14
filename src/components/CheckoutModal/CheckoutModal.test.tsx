7a258f218fddecf42aa9477a2674cc9297eaa0f6
Igor Pessoal <castromann@live.com>
2024-02-14 15:34:54 -0300

    test(checkout-modal): fix tests

--- src/components/CheckoutModal/CheckoutModal.test.tsx
+++ src/components/CheckoutModal/CheckoutModal.test.tsx
@@ -1,11 +1,31 @@
 import { renderWithProvider } from "@test/render";
 import { CheckoutModal } from ".";
 import { act } from "@testing-library/react-native";
+import { RootStackScreenComponent } from "@src/routes/types";
+
+jest.mock("@tanstack/react-query", () => {
+  const original = jest.requireActual("@tanstack/react-query");
+
+  return {
+    ...original,
+    useMutation: () => ({
+      mutate: jest.fn(),
+      isLoading: false,
+      error: {},
+    }),
+  };
+});
+
+const mockNavigation = {} as RootStackScreenComponent<"Cart">["navigation"];
 
 describe("<CheckoutModal />", () => {
   it("renders correctly", () => {
     const { getByTestId } = renderWithProvider(
-      <CheckoutModal isOpen={true} onClose={jest.fn()} />,
+      <CheckoutModal
+        isOpen={true}
+        onClose={jest.fn()}
+        navigation={mockNavigation}
+      />,
     );
 
     expect(getByTestId("checkout-modal")).toBeDefined();
@@ -14,7 +34,11 @@ describe("<CheckoutModal />", () => {
   it("should close modal", () => {
     const onClose = jest.fn();
     const { queryByTestId } = renderWithProvider(
-      <CheckoutModal isOpen={false} onClose={onClose} />,
+      <CheckoutModal
+        isOpen={false}
+        onClose={onClose}
+        navigation={mockNavigation}
+      />,
     );
 
     act(() => {
